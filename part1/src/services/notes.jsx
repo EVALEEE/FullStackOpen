b9d97e71a192ddb61b22754bfb8e28b02039e7ea
Eva Lee <u7829183@anu.edu.au>
2025-01-21 21:18:03 +0800

    update

--- part1/src/services/notes.jsx
+++ part1/src/services/notes.jsx
@@ -3,6 +3,7 @@ const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
     return axios.get(baseUrl)
+    //这块先不改
 }
 
 const create = newObject => {
@@ -18,9 +19,14 @@ const update = (id, newObject) => {
 }
 
 export default {
-    getAll: getAll,
-    create: create,
-    update: update
+    // 对象定义中冒号左边的标签是对象的键，而它右边的是在模块中定义的变量。
+    // getAll: getAll,
+    // create: create,
+    // update: update
+    // 由于键的名字和分配的变量是一样的，我们可以用更紧凑的语法来写对象定义。
+    getAll,
+    create,
+    update
 }
 // 该模块返回一个对象，该对象有三个函数（getAll、create和update）作为其属性，处理笔记。这些函数直接返回axios方法所返回的 promise 。
 
