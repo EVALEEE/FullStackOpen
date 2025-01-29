1126a09f36793470f85abbe2c04af7e8b91fba4a
Eva Lee <u7829183@anu.edu.au>
2025-01-29 17:13:46 +0800

    update

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,32 +1,49 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstackopen:${password}@cluster0.biysl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://fullstackopen:${password}@cluster0.biysl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+    content: String,
+    important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is easy',
-  important: true,
+    content: 'HTML is easy',
+    important: true,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
+// 保存笔记
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+//程序会打印出数据库中存储的所有笔记
+//参数是一个表示搜索条件的对象，这里我们传递了一个空对象，因此返回的是数据库中的所有笔记
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
 })
+
+// Note.find({important: true}).then(result => {
+//     result.forEach(note => {
+//         console.log(note)
+//     })
+//     mongoose.connection.close()
+// })
