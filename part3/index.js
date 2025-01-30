daffe32b2f4c519a51bde983afd9c420b1b845c2
Eva Lee <u7829183@anu.edu.au>
2025-01-30 17:53:22 +0800

    add errorHandler and unknownEndpoint

--- part3/index.js
+++ part3/index.js
@@ -32,61 +32,64 @@ app.use(express.json()) // 为了方便地访问数据，我们需要 express js
 //定义两个通往应用的 路径 
 //第一个定义了一个事件处理程序，用来处理对应用 / 根的 HTTP GET 请求。
 app.get('/', (request, response) => {
-    //第一个 request 参数包含 HTTP 请求的所有信息
-    //第二个 response 参数用于定义如何对请求进行响应。
-    response.send('<h1>Hello World!</h1>')
-    // 调用该方法使服务器响应 HTTP 请求，发送一个响应，其中包含传递给 send 方法的字符串
+  //第一个 request 参数包含 HTTP 请求的所有信息
+  //第二个 response 参数用于定义如何对请求进行响应。
+  response.send('<h1>Hello World!</h1>')
+  // 调用该方法使服务器响应 HTTP 请求，发送一个响应，其中包含传递给 send 方法的字符串
 })
 
 
 // 获取全部Notes
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
-
 
 //根据id获取对应的Note
 app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
         response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
 
-
-
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-
-
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+  //如果next没有参数被调用，那么执行将简单地移动到下一个路由或中间件。
+  //如果next函数带有参数被调用，那么执行将继续到错误处理中间件。
+})
 
 
 //创建新的Note
 app.post('/api/notes', (request, response) => {
-    console.log('Received POST request to /api/notes')  // 新增
-    console.log('Request body:', request.body)  // 新增
-    const body = request.body //如果没有 json-parser，body 属性将是未定义的
+  console.log('Received POST request to /api/notes')  // 新增
+  console.log('Request body:', request.body)  // 新增
+  const body = request.body //如果没有 json-parser，body 属性将是未定义的
 
-    if (body.content === undefined) {
-        console.log('Content missing in request')  // 新增
-        return response.status(400).json({ error: 'content missing' })
-    }
+  if (body.content === undefined) {
+    console.log('Content missing in request')  // 新增
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-    console.log('Saving note:', note)
+  console.log('Saving note:', note)
 
-    note.save()
+  note.save()
     .then(savedNote => {
       response.json(savedNote)
     })
@@ -99,22 +102,47 @@ app.post('/api/notes', (request, response) => {
 
 //更新笔记
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
-  
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updatedNote => {
-        response.json(updatedNote)
-        console.log('Updated note:', updatedNote)
-      })
-      .catch(error => next(error))
-  })
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+      console.log('Updated note:', updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+
+// error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
