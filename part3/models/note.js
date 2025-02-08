0aece127104462d131a52ad91cba7a0d2f6dd68b
Eva Lee <u7829183@anu.edu.au>
2025-02-08 13:58:41 +0800

    add bcrypt

--- part3/models/note.js
+++ part3/models/note.js
@@ -7,6 +7,10 @@ const noteSchema = new mongoose.Schema({
     minlength: 3
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 noteSchema.set('toJSON', {
