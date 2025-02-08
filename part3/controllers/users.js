162d2ed1bca1a16a8369b14c74738a952633e961
Eva Lee <u7829183@anu.edu.au>
2025-02-08 18:37:55 +0800

    finished part4c

--- part3/controllers/users.js
+++ part3/controllers/users.js
@@ -19,7 +19,16 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    //Mongoose的连接
+    // const users = await User.find({}).populate('notes')
+    //提供给 populate 方法的参数定义了在user文档的notes字段中
+    //引用note对象的ids(id的复数)将被替换为被引用的note文档
+
+    const users = await User
+        .find({}).populate('notes', { content: 1})
+    //使用populate参数来选择我们想从文档中包含的字段。
+    //字段的选择是通过Mongo的语法完成的。
+
     response.json(users)
 })
 
