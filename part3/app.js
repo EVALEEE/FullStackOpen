298e39656bc10d4fe84187a07b0f73d006c37101
Eva Lee <u7829183@anu.edu.au>
2025-02-08 17:26:14 +0800

    update

--- part3/app.js
+++ part3/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')//库处理了引擎盖下的一切。如果在as
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger') //const { info, error } = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -26,6 +27,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part3/controllers/notes.js
+++ part3/controllers/notes.js
@@ -3,38 +3,16 @@
 //它可以用来在一个地方定义 "相关的路由"，它通常被放在自己的模块中。
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-    // Note.find({}).then(notes => {
-    //     response.json(notes)
-    // })
     const notes = await Note.find({})
     response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response) => {
-    // Note.findById(request.params.id)
-    //     .then(note => {
-    //         if (note) {
-    //             response.json(note)
-    //         } else {
-    //             response.status(404).end()
-    //         }
-    //     })
-    //     .catch(error => next(error))
-
-    // try {
-    //     const note = await Note.findById(request.params.id)
-    //     if (note) {
-    //         response.json(note)
-    //     } else {
-    //         response.status(404).end()
-    //     }
-    // } catch (exception) {
-    //     next(exception)
-    // }
-
     const note = await Note.findById(request.params.id)
+
     if (note) {
         response.json(note)
     } else {
@@ -45,35 +23,23 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const user = await User.findById(body.userId)
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
+        user: user.id
     })
-
-    // note.save()
-    //     .then(savedNote => {
-    //         response.status(201).json(savedNote)
-    //     })
-    //     .catch(error => next(error))
-
-    // try {
-    //     const savedNote = await note.save()
-    //     response.status(201).json(savedNote)
-    // } catch (exception) {
-    //     next(exception)
-    // }
 
     //使用了express-async-errors库，所以不需要try-catch块
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     response.status(201).json(savedNote)
 })
 
 notesRouter.delete('/:id', async (request, response) => {
-    // Note.findByIdAndDelete(request.params.id)
-    //     .then(() => {
-    //         response.status(204).end()
-    //     })
-    //     .catch(error => next(error))
     await Note.findByIdAndDelete(request.params.id)
     response.status(204).end()
 })
