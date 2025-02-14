b2cfa92d67e2d1aae10237476a7cbb64f4ac87e2
Eva Lee <u7829183@anu.edu.au>
2025-02-14 16:38:31 +0800

    add user-event package

--- part1/src/components/Note.test.jsx
+++ part1/src/components/Note.test.jsx
@@ -7,10 +7,26 @@ test('renders content', () => {
         important: true
     }
 
+    //方法1:
     //使用了由react-testing-library提供的 render 函数来渲染组件
     render(<Note note={note} />)
 
+    screen.debug()//可用于将组件的 HTML 打印到终端
+
     const element = screen.getByText('Component testing is done with react-testing-library')
+    
+    screen.debug(element)
+
     //使用Vitest的 expect 命令来检查元素的存在性
     expect(element).toBeDefined()
+
+    //=====================================================
+
+    //方法2:
+    // const { container } = render(<Note note={note} />)
+
+    // const div = container.querySelector('.note')
+    // expect(div).toHaveTextContent(
+    //     'Component testing is done with react-testing-library'
+    // )
 })
