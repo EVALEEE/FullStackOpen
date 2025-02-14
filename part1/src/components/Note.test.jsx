07aea5439fda531c4d9024c1f400ca7e6666326d
Eva Lee <u7829183@anu.edu.au>
2025-02-14 16:56:12 +0800

    update

--- part1/src/components/Note.test.jsx
+++ part1/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -14,7 +15,7 @@ test('renders content', () => {
     screen.debug()//可用于将组件的 HTML 打印到终端
 
     const element = screen.getByText('Component testing is done with react-testing-library')
-    
+
     screen.debug(element)
 
     //使用Vitest的 expect 命令来检查元素的存在性
@@ -29,4 +30,27 @@ test('renders content', () => {
     // expect(div).toHaveTextContent(
     //     'Component testing is done with react-testing-library'
     // )
+})
+
+test('clicking the button calls event handler once', async () => {
+    const note = {
+        content: 'Component testing is done with react-testing-library',
+        important: true
+    }
+
+    const mockHandler = vi.fn()//事件处理程序是一个使用 Vitest 定义的 mock 函数
+
+    render(
+        <Note note={note} toggleImportance={mockHandler} />
+    )
+
+    const user = userEvent.setup()//session 启动以与呈现的组件进行交互
+    
+    //测试基于呈现组件的文本找到按钮并单击元素
+    const button = screen.getByText('make not important')
+    //单击使用 userEvent 库的 click 方法进行
+    await user.click(button)
+
+    //测试的期望使用 toHaveLength 来验证mock 函数已被调用一次
+    expect(mockHandler.mock.calls).toHaveLength(1)
 })
