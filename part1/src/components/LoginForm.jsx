4e5f72e9e2e0065d4686b23b30456b048ccb3a9d
Eva Lee <u7829183@anu.edu.au>
2025-02-15 21:48:47 +0800

    update

--- part1/src/components/LoginForm.jsx
+++ part1/src/components/LoginForm.jsx
@@ -13,6 +13,7 @@ const LoginForm = ({
                 <div>
                     username
                     <input
+                        id='username'
                         value={username}
                         onChange={handleUsernameChange}
                         name="username"
@@ -21,13 +22,14 @@ const LoginForm = ({
                 <div>
                     password
                     <input
+                        id='password'
                         type="password"
                         value={password}
                         onChange={handlePasswordChange}
                         name="password"
                     />
                 </div>
-                <button type="submit">login</button>
+                <button id="login-button" type="submit">login</button>
             </form>
         </div>
     )
